af491b0ed6382a09726cd0cf379ac4110563ad33
thony-cb <118865307+thony-cb@users.noreply.github.com>
2023-06-01 19:25:55 -0400

    Update Exercise05_09.java

--- Exercise05_09.java
+++ Exercise05_09.java
@@ -3,3 +3,37 @@
 
 // My code
 
+import java.util.Scanner;
+
+public class Exercise05_09 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String topName = "";
+		String topNameTwo = "";
+		int topScoreOne = 1;
+		int topScoreTwo = 1;
+		System.out.println("Enter the number of students:");
+		int num = input.nextInt();
+        input.nextLine();
+		for (int i = 0; i < num; i++) {
+			System.out.println("Enter a student name:");
+			String kid = input.nextLine();
+			System.out.println("Enter a student score:");
+			int score = input.nextInt();
+			input.nextLine();
+			if (score > topScoreOne) {
+				topScoreTwo = topScoreOne;
+                topNameTwo = topName;
+                topScoreOne = score;
+                topName = kid;
+			} else if (score > topScoreTwo) {
+				topScoreTwo = score;
+				topNameTwo = kid;
+			}
+		}
+
+		System.out.println("Top two students:\n" + topName + "score is" + topScoreOne + "\n" + topNameTwo + "score is"
+				+ topScoreTwo);
+
+	}
+}
