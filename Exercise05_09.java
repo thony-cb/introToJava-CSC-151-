d6322e3526a8881f93ce738e71102cd3162c5eb7
thony-cb <118865307+thony-cb@users.noreply.github.com>
2023-06-01 19:05:38 -0400

    Create Exercise05_09.java

---
+++ Exercise05_09.java
@@ -0,0 +1,5 @@
+// (Find the two highest scores) 
+// Write a program that prompts the user to enter the number of students and each student’s name and score, and finally displays the student with the highest score and the student with the second-highest score. Use the next()method in the Scanner class to read a name rather than using the nextLine()method. Assume that the number of students is at least 2.
+
+// My code
+
